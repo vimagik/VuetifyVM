a09ece2b9ff995ab5866205d7e997a53aa54cdf4
UbuntuX <vimagik@gmail.com>
2021-11-14 01:46:11 +0300

    Lesson 2

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>
-    <h1>Шапка</h1>
-    <nuxt />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Vuetify dushboard</v-toolbar-title>
+      <v-spacer />
+      <v-btn text rounded>
+        Home
+      </v-btn>
+      <v-btn text rounded>
+        Login
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <nuxt />
+    </v-main>
+    <my-footer />
+  </v-app>
 </template>
 
 <script>
+import MyFooter from '~/components/MyFooter'
+
 export default {
+  components: {
+    MyFooter
+  },
   data () {
     return {}
   }
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,48 @@
 <template>
-  <div>
-    <h1>Привет</h1>
-  </div>
+  <v-app>
+    <v-card width="400" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">
+          Login
+        </h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+          <v-text-field
+            label="Password"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="showPassword ? 'text' : 'password'"
+            @click:append="showPasswordClick"
+          />
+        </v-form>
+      </v-card-text>
+      <v-divider />
+      <v-card-actions>
+        <v-btn color="success">
+          Register
+        </v-btn>
+        <v-spacer />
+        <v-btn color="info">
+          Login
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-app>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      showPassword: false
+    }
+  },
+  methods: {
+    showPasswordClick () {
+      this.showPassword = !this.showPassword
+    }
+  }
+}
+</script>
